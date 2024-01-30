00eca50147c5f1de17dcc840e57a8bdbb23cebfe
Nicolas Alonzo <alonzonicolas25@gmail.com>
2024-01-29 23:35:47 -0300

    Suppliers

---
+++ CapaDatos/CD_Proveedor.cs
@@ -0,0 +1,169 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CD_Proveedor
+    {
+        public List<Proveedor> Listar()
+        {
+            List<Proveedor> lista = new List<Proveedor>();
+
+            using (SqlConnection oConexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("select IdProveedor,Documento,RazonSocial,Correo,Telefono,Estado from PROVEEDOR");
+
+                    SqlCommand cmd = new SqlCommand(query.ToString(), oConexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oConexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+
+                        while (dr.Read())
+                        {
+                            lista.Add(new Proveedor()
+                            {
+                                idProveedor = Convert.ToInt32(dr["idProveedor"]),
+                                documento = dr["documento"].ToString(),
+                                razonSocial = dr["razonSocial"].ToString(),
+                                correo = dr["correo"].ToString(),
+                                telefono = dr["telefono"].ToString(),
+                                estado = Convert.ToBoolean(dr["estado"])
+                            });
+                        }
+
+                    }
+                }
+                catch (Exception)
+                {
+                    lista = new List<Proveedor>();
+                }
+            }
+
+            return lista;
+        }
+
+
+
+        public int Registrar(Proveedor obj, out string Mensaje)
+        {
+            int idProveedorGenerado = 0;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cadena))
+                {
+
+                    SqlCommand cmd = new SqlCommand("SP_RegistrarProveedor".ToString(), oConexion);
+                    cmd.Parameters.AddWithValue("Documento", obj.documento);
+                    cmd.Parameters.AddWithValue("RazonSocial", obj.razonSocial);
+                    cmd.Parameters.AddWithValue("Correo", obj.correo);
+                    cmd.Parameters.AddWithValue("Telefono", obj.telefono);
+                    cmd.Parameters.AddWithValue("Estado", obj.estado);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    idProveedorGenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                idProveedorGenerado = 0;
+                Mensaje = ex.Message;
+            }
+
+            return idProveedorGenerado;
+        }
+
+
+        public bool Editar(Proveedor obj, out string Mensaje)
+        {
+            bool respuesta = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cadena))
+                {
+
+                    SqlCommand cmd = new SqlCommand("SP_ModificarProveedor".ToString(), oConexion);
+                    cmd.Parameters.AddWithValue("idProveedor", obj.idProveedor);
+                    cmd.Parameters.AddWithValue("Documento", obj.documento);
+                    cmd.Parameters.AddWithValue("RazonSocial", obj.razonSocial);
+                    cmd.Parameters.AddWithValue("Correo", obj.correo);
+                    cmd.Parameters.AddWithValue("Telefono", obj.telefono);
+                    cmd.Parameters.AddWithValue("Estado", obj.estado);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta = false;
+                Mensaje = ex.Message;
+            }
+
+            return respuesta;
+        }
+
+
+        public bool Eliminar(Proveedor obj, out string Mensaje)
+        {
+            bool respuesta = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cadena))
+                {
+
+                    SqlCommand cmd = new SqlCommand("SP_EliminarProveedor".ToString(), oConexion);
+                    cmd.Parameters.AddWithValue("idProveedor", obj.idProveedor);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta = false;
+                Mensaje = ex.Message;
+            }
+
+            return respuesta;
+        }
+    }
+}
--- CapaPresentacion/frmProveedores.Designer.cs
+++ CapaPresentacion/frmProveedores.Designer.cs
@@ -28,34 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(307, 210);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(341, 31);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Formulario de Proveedores";
             // 
             // frmProveedores
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1057, 602);
             this.Name = "frmProveedores";
             this.Text = "frmProveedores";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
     }
 }
